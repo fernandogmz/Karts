635b1640b62522cd262196599041f0a96505c90d
Fernando Gomez <fernando.gomez1401@gmail.com>
2023-05-21 21:16:43 +0200

    Feature: Menu content

--- src/State/States/MenuState.h
+++ src/State/States/MenuState.h
@@ -6,7 +6,17 @@
 
 class MenuState: public State{
 public:
+    // Fonts
+    ofTrueTypeFont gameplayS, gameplayM, gameplayL, gameplayXL;
+    
     MenuState(Game *game): State(game, "Menu State"){
+        
+        // Fonts
+        ofTrueTypeFont::setGlobalDpi(72);
+        gameplayS.load("BirdyGame.ttf", 24);
+        gameplayM.load("BirdyGame.ttf", 48);
+        gameplayL.load("BirdyGame.ttf", 72);
+        gameplayXL.load("BirdyGame.ttf", 128);
     };
     
     ~MenuState(){};
@@ -14,6 +24,11 @@ public:
     void update();
     void draw();
     void next();
+    void keyPressed(int key);
+    
+    void drawCenteredText(ofTrueTypeFont font, float y, string text);
+    void drawTextXY(ofTrueTypeFont font, float x, float y, string text);
+    
         
 };
 #endif 
